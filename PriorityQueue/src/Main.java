6776ad2e6b60c5bfacdfa63a736655a5b2937839
devshehan <shehaan.avishka00@gmail.com>
2023-08-25 17:47:55 +0530

    priority queue data structure

---
+++ PriorityQueue/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        *   Priority Queue => It's a class like LinkedList
+        * */
+
+        Queue<Double> studentGpa = new PriorityQueue<>(Collections.reverseOrder());
+
+        studentGpa.offer(4.1);
+        studentGpa.offer(3.5);
+        studentGpa.offer(3.0);
+        studentGpa.offer(2.8);
+
+        while(!studentGpa.isEmpty()){
+            System.out.println(studentGpa.poll());
+        }
+    }
+}
