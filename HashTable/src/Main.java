043d71e6c03a202992312e2e571f2d849a16949c
devshehan <shehaan.avishka00@gmail.com>
2023-09-06 11:07:31 +0530

    hash table

--- HashTable/src/Main.java
+++ HashTable/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Hashtable;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        // most suitable for large data set
+        // best case O(n), worst case O(n)
+        // different type of data types use different type of hashcode formula.
+
+        Hashtable<Integer,String> hashtable = new Hashtable<>(100);
+
+        hashtable.put(12,"Optimus Prime");
+        hashtable.put(10,"Bee");
+        hashtable.put(1,"Ratchet");
+        hashtable.put(2,"Sam");
+
+        hashtable.remove(1);
+
+        for(Integer k : hashtable.keySet()){
+            System.out.println(k.hashCode()+ "\t" + k + "\t" + hashtable.get(k));
+        }
+
     }
 }
